fab7eb6403626e5d25648ad0a34526c66166d2c6
AxelMathieu94 <104462967+AxelMathieu94@users.noreply.github.com>
2022-05-03 21:30:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,7 @@ function Compute() {
 
     var interest = principal * years * rate / 100;
     
-    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>, <br> at an interest rate of <mark>"+rate+"</mark>. <br> You will receive an amount of <mark>"+interest+"</mark> <br> in the year <mark>"+finalyear</mark>;
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+"</mark>, <br> at an interest rate of <mark>"+rate+"</mark>. <br> You will receive an amount of <mark>"+interest+"</mark> <br> in the year <mark>"+finalyear+"</mark>";
 }
+
+        
