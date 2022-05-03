73b74a8f4edfd9d78953269b2fd4f4a78a909f1b
AxelMathieu94 <104462967+AxelMathieu94@users.noreply.github.com>
2022-05-03 20:12:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function updateRate(){
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rate;
+}
+
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+    var finalyear = new Date().getFullYear()+parseInt(years);
+
+    var interest = amount * years * rate / 100;
+    
+    document.getElementById("result").innerHTML = "If you deposit "+principal+", <br> at an interest rate of "+rate+". <br> You will receive an amount of "+(parseInt(amount)+interest)+" <br> in the year "+finalyear;
 }
-    function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-}
-
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-
-
-        
